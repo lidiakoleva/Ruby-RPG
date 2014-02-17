557da3b6d6b545d0c924d53bdd72698f008e6cb3
xswordsx <xswordsx@gmail.com>
2014-02-17 22:55:27 +0200

    Refactor and redesign the `Item` class
    Now there are different classes for different types of items instead of using the ``@type`` variable.

--- lib/item.rb
+++ lib/item.rb
@@ -1,29 +1,58 @@
 require "constants.rb"
 
 class Item
-  attr_reader :name, :description, :stats, :type
-  def initialize(name='', desc='', stats={}, type=:consumable)
-    @name = (name.is_a? String) ? name : "???"
-    @description = (desc.is_a? String) ? desc : "IDK LOL"
-    @stats = (stats.is_a? Hash) ? stats : {}
-    @equipped = false
-    @type = type
+  attr_reader :name, :description, :stats
+
+  def initialize(name, description, stats)
+    @name = name || "Unknown item"
+    @description = description || ''
+    @stats = stats || {}
   end
 
-  def equip
-    if @equipped == false
-      @equipped = true
-      :equip
-    else
-      :already_equipped
-    end
-  end
-
-  def == other
-    (@name == other.name and @type == other.type and @stats == other.stats)
-  end
-
-  def equipped?
-    @equipped
+  def ==(other)
+    (@name == other.name and @stats == other.stats)
   end
 end
+
+
+class Consumable < Item
+  attr_reader :name, :description, :stats, :stack
+
+  def initialize(name, description, stats, stack)
+    super(name, description, stats)
+    @stack = stack
+  end
+
+  def add
+    @stack += 1
+  end
+
+  def drop(count = 1)
+    @stack -= count
+  end
+
+  def empty?
+    @stack < 1
+  end
+
+  def equip(position)
+    #TODO -- not sure if needed
+  end
+
+end
+
+class Armor < Item
+  attr_reader :name, :description, :stats
+  def initialize(name, description, stats)
+    super(name, description, stats)
+  end
+end
+
+class Weapon < Item
+  attr_reader :name, :description, :stats
+
+  def initialize(name, description, stats)
+    super(name, description, stats)
+  end
+end
+
