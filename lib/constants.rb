965662851524629c3789b82bd0e1b7c41e30c2d5
xswordsx <xswordsx@gmail.com>
2014-02-18 00:53:48 +0200

    Add `DIRECTIONS`

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,11 +1,9 @@
 module Players
-  BASIC_STATS = {
-    :hp => 80,
-    :armor => 0,
-    :damage => 12,
-    :mana => 60
-  }
+  BASIC_STATS = {:hp => 80, :armor => 0, :damage => 12, :mana => 60}
   MAX_LOAD = 25
+
+  DIRECTIONS = {:up => [0, 1], :down => [0, -1],
+                :left => [-1, 0], :right => [1, 0]}
 end
 
 module Colors
