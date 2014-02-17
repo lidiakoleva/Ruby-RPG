f33edf6ddf8d609ae3b6a974f5022d1922544e12
xswordsx <xswordsx@gmail.com>
2014-02-18 01:10:37 +0200

    Add pending expectations for `Player`
    We all expect great deeds from him!

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,49 +2,69 @@ require_relative "../lib/player.rb"
 require_relative "../lib/item.rb"
 require_relative "../lib/constants.rb"
 
-describe "Player" do
+describe Player do
   let (:player) { Player.new "Doctor Who" }
   let (:sword) {
     Weapon.new "The sword of epicness",
       'the best sword in the world!',
       {:damage => 20}}
 
-  it "has a name" do
-    player.name.should be_kind_of String
-  end
+  context "basic information" do
 
-  it "has expirience" do
-    player.should respond_to :xp
-  end
+    it "has a name" do
+      player.name.should be_kind_of String
+    end
 
-  it "has a level" do
-    player.should respond_to :level
-  end
+    it "has expirience" do
+      player.should respond_to :xp
+    end
 
-  it "has items" do
-    player.should respond_to :inventory
-  end
+    it "has a level" do
+      player.should respond_to :level
+    end
 
-  it "can pick up items (if the inventory is not full)" do
-    unless player.inventory_full?
-      player.pick_up(sword).should be == :pick_up
+    it "has stats" do
+      player.stats.should be_kind_of Hash
     end
   end
 
-  it "has stats" do
-    player.stats.should be_kind_of Hash
+  context "can interact with items" do
+
+    it "has items" do
+      player.should respond_to :inventory
+    end
+
+    it "picks up items (if the inventory is not full)" do
+      unless player.inventory_full?
+        player.pick_up(sword).should be == :pick_up
+      end
+    end
+
+    it "drops an item" do
+      me = Player.new "CodeMonkey"
+      me.pick_up(sword)
+      me.load.should be == 1
+      me.drop(sword).should be == :item_dropped
+      me.load.should be == 0
+    end
   end
 
-  it "can drop an item" do
-    me = Player.new "CodeMonkey"
-    me.pick_up sword
-    me.drop(sword).should be == :item_dropped
+  context "can interact with mobs:" do
+    it "fights monsters"
+
+    it "dies"
+
+    it "levels up"
+
+    it "gains expirience"
   end
 
   it "can move around" do
     player.move(:up).should be == :player_moved
-    #player.move(5).should be == :unable_to_move
+    player.move(5).should be == :unable_to_move
   end
+
+
 
 
 end
