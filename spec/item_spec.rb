00aae9048671d8ed59a2bf30e3e3322f443e8ac1
xswordsx <xswordsx@gmail.com>
2014-02-18 18:03:29 +0200

    Fix some typos and add a few tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,15 +1,8 @@
+
 require_relative "../lib/item.rb"
-require_relative "../lib/player.rb"
 
-describe "Item" do
+describe Item do
   let (:potion) {Item.new "Health potion", 'delicious juice', {:current_hp => 15}}
-
-  let (:sword) {
-  Item.new "The sword of epicness",
-    'the best sword in the world!',
-    {:damage => 20}}
-
-  let(:player) { Player.new "John" }
 
   it "has a name" do
     potion.name.should be_kind_of String
