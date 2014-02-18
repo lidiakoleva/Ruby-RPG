7d418f77b025af8987337690e180f2f87ba9ca85
xswordsx <xswordsx@gmail.com>
2014-02-18 16:57:36 +0200

    Implement `World#create_map` the method creates the world map and sets the position of the player

--- lib/world.rb
+++ lib/world.rb
@@ -1,17 +1,38 @@
-require_relative 'bmp_reader.rb'
+require 'bmp_reader.rb'
+require 'constants.rb'
 
 class World
 
   attr_reader :map
 
-  def initialize(map)
-    @map = create_map(map)
+  def initialize(level, player_name)
+    @map = nil
+    @player = nil
+    create_map(level, player_name)
   end
 
   private
-  def create_map(level)
+  def create_map(level, player_name)
     bitmap = BMPReader.new ("../data/levels/#{level}.bmp")
-    #TODO
+    @map = Array.new(bitmap.height) {Array.new(bitmap.width)}
+    (0..bitmap.height).each  do |j|
+      (0..bitmap.width).each do |i|
+
+        case bitmap[i,j]
+        when Colours::GREEN
+          @map[i][j] = Tile.new
+        when Colours::GREY
+          @map[i][j] = Wall.new
+        when Colours::Black
+          @map[i][j] = Tile.new(true, NPC.new)
+        when Colours::BLUE
+          @map[i][j] = Water.new
+        when Colours::WHITE
+          @map[i][j] = Tile.new
+          @player = Player.new(player_name, i, j)
+        end
+      end
+    end
   end
 
 end
