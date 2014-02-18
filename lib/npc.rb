958a53bf3348429ba024b5ed13d94295fd17f840
xswordsx <xswordsx@gmail.com>
2014-02-18 18:04:09 +0200

    Introduce the skeleton of the NPCs (mobs)

--- lib/npc.rb
+++ lib/npc.rb
@@ -1,2 +1,14 @@
 class NPC
+  attr_reader :name, :stats, :xp
+  def initialize(name, stats = {}, xp = 100)
+    @name = name
+    @stats = stats
+    @xp = xp
+  end
+
+  def kill
+    stats[:current_hp] = 0
+    @xp
+  end
+
 end
--- spec/npc_spec.rb
+++ spec/npc_spec.rb
@@ -2,26 +2,27 @@ require_relative '../lib/npc.rb'
 
 describe NPC do
   let(:npc) {NPC.new ("Cookie Monster")}
+
   context "basic stats" do
     it "has a name" do
       npc.should respond_to :name
     end
 
-    it "has stats"
+    it "has stats" do
       npc.should respond_to :stats
     end
 
-    it "has experience that it can give off (upon death)"
+    it "has experience that it can give off (upon death)" do
       npc.kill.should be_kind_of Integer
     end
   end
 
   context "can fight:" do
 
-    it "can die"
+    it "can die" do
       npc.should respond_to :kill
     end
-    
+
     it "receives damage"
 
     it "damages the player"
