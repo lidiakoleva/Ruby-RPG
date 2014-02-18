326693786613b710ec7b2fde8b196f1aa460be93
xswordsx <xswordsx@gmail.com>
2014-02-18 16:23:21 +0200

    Add colours for the player and the mobs

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,8 @@
 module Colors
+  #Note: BMP saves the colours in BGR instead of RGB!
   GREEN = '00c864'.freeze
   GREY = '919191'.freeze
   BLUE = '9b7805'.freeze
+  BLACK = '000000'.freeze
+  WHITE = 'ffffff'.freeze
 end
