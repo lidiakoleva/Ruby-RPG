7ebe442953f7755917258b73af53497212ea926b
xswordsx <xswordsx@gmail.com>
2014-02-19 20:41:46 +0200

    Add NPC intercations with Player

--- lib/npc.rb
+++ lib/npc.rb
@@ -1,14 +1,28 @@
 class NPC
   attr_reader :name, :stats, :xp
-  def initialize(name, stats = {}, xp = 100)
+  def initialize(name, stats = {}, xp = 100, item = nil)
     @name = name
     @stats = stats
     @xp = xp
+    @current_hp = @stats[:hp]
+    @item = item
   end
 
-  def kill
-    stats[:current_hp] = 0
-    @xp
+  def dead?
+    @current_hp < 1
+  end
+
+  def loot
+    {:xp => @xp, :item => @item}
+  end
+
+  def damage
+    (@stats[:damage] + @xp * 0.1).round
+  end
+
+  def recieve_damage(player)
+    damage_reduction = (1.00 - 100 / (100.00 + @stats[:armour])) * 100
+    @current_hp = @current_hp - (player.damage * damage_reduction).round
   end
 
 end
--- spec/npc_spec.rb
+++ spec/npc_spec.rb
@@ -12,13 +12,14 @@ describe NPC do
       npc.should respond_to :stats
     end
 
-    it "has experience that it can give off (upon death)" do
-      npc.kill.should be_kind_of Integer
+    it "has experience and items that it can give off (upon death)" do
+      npc.loot[:xp].should be >= 0
+      npc.loot[:item].should be_kind_of Item unless npc.loot[:item].nil?
     end
   end
 
-  it "can die" do
-    npc.should respond_to :kill
-  end
+  it "has a chance to drop an item"
+
+  it "gives off experience when it dies (based on it's own)"
 
 end
