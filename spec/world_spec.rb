ee359e4f5af801f044b0d319921e1171a5309c27
xswordsx <xswordsx@gmail.com>
2014-02-19 20:42:29 +0200

    `World` now has operators [] and []=

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -5,6 +5,11 @@ describe World do
   let(:path) {String.new("/home/latunov/Desktop/ruby_proj/data/levels/1.bmp")}
   let (:world) {World.new(path, "Random Hero")}
 
+  it "can be initialized" do
+    w = World.new(path, "name")
+    w.should be_kind_of World
+  end
+
   it "has a map" do
     world.should respond_to :map
   end
@@ -14,7 +19,8 @@ describe World do
   end
 
   it "map has tiles" do
-    world.map.each {|x| x.any? {|tile| tile.should be_kind_of Tile}}
+    world = World.new("data/levels/1.bmp", "")
+    world[0, 0].should be_kind_of Tile
   end
 
   it "map has NPCs" do
