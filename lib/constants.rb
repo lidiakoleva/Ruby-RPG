528d2d384345cd8c19e12d04d50c1d21cac1c1de
xswordsx <xswordsx@gmail.com>
2014-02-19 20:35:49 +0200

    Move module `Maps` in `World` class

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,7 +1,3 @@
-require 'tile.rb'
-require 'player.rb'
-require 'npc.rb'
-
 module Colours
   #Note: BMP saves the colours in BGR instead of RGB!
   GREEN = '00c864'.freeze
@@ -9,16 +5,4 @@ module Colours
   BLUE = '9b7805'.freeze
   BLACK = '000000'.freeze
   WHITE = 'ffffff'.freeze
-end
-
-module Map
-  WORLD = {Colours::GREEN => Tile,
-           Colours::GREY => Wall,
-           Colours::BLUE => Water,
-           Colours::BLACK => NPC,
-           Colours::WHITE => Player}.freeze
-
-  NPC_STATS = {:name => ['Giant Rat', 'Goo Blob', 'Skeleton', 'Merman', 'Elf'],
-               :stats => [{}],
-               :xp => [240, 250, 260, 270]}.freeze
 end
