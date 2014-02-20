1ed19d3fae01d055bd825a80440b89a4b342fda8
xswordsx <xswordsx@gmail.com>
2014-02-20 19:21:37 +0200

    HUGE Commit - read logs below!!
    + Implement method `interact` on a moving level - you can move around the world
    + Add method `render_pixel` - renders pixel[x,y] to be `pixel_type`
    + Add methods [`inside_world?`, `move_player`, `add_titles`] - pretty self-explainatory
    + Remove method `render_player` - it's a subcase of `render_pixel`

--- lib/terminal_gui.rb
+++ lib/terminal_gui.rb
@@ -14,7 +14,7 @@ class TerminalGUI
     @world = nil
     @main_window = nil
     @subwindow = nil
-    @quit = false
+    @run = true
 
     @render_palette = render_palette.merge @@render_palette
     @lvl_path = lvl_path
@@ -34,6 +34,26 @@ class TerminalGUI
   private
 
   def interact
+    Curses.noecho
+    @subwindow.keypad(true)
+
+    keys = Curses::Key
+    @directions = {keys::UP => :up, keys::DOWN => :down,
+                   keys::LEFT => :left, keys::RIGHT => :right}
+
+    loop do
+      key = @subwindow.getch
+      break if key.equal?(27)
+      if @directions.include? key
+        move_player(@directions[key], @subwindow)
+      end
+      @main_window.refresh
+    end
+  end
+
+  def inside_world?(x, y)
+    0.upto(@world.width.pred).include? x and
+    0.upto(@world.height.pred).include? y
   end
 
   def render_player(window, pair)
@@ -44,6 +64,13 @@ class TerminalGUI
     window.refresh
   end
 
+  def render_pixel(window, x, y, pixel_type)
+    window.setpos(y, x * 2)
+    window.attron(Curses::color_pair(@pairs[pixel_type]))
+    window << @render_palette[pixel_type]
+    window.attroff(Curses::color_pair(@pairs[pixel_type]))
+  end
+
   def check_map_size
     if Curses::lines < @world.height + 3
       print "Map too large to be loaded!\n"
@@ -52,7 +79,19 @@ class TerminalGUI
     end
   end
 
-  def move_player(direction)
+  def move_player(direction, window)
+    x, y = @world.player.x, @world.player.y
+
+    dir_vector = @world.player.directions(direction)
+    x_next, y_next = x + dir_vector[0], y + dir_vector[1]
+
+    if inside_world?(x_next, y_next) and
+      @world[x_next, y_next].instance_of? Tile
+      @world.player.move(direction)
+
+      render_pixel(window, x, y, "Tile")
+      render_pixel(window, x_next, y_next, "Player")
+    end
   end
 
   def initialize_color_pairs
@@ -64,8 +103,8 @@ class TerminalGUI
     Curses::init_pair(5, Curses::COLOR_BLUE, Curses::COLOR_GREEN)
   end
 
-  def add_titles(heading_1, heading_2, pair)
-    @main_window.attron(Curses::color_pair(pair["Title"]))
+  def add_titles(heading_1, heading_2)
+    @main_window.attron(Curses::color_pair(@pairs["Title"]))
 
     @main_window.setpos(2, (Curses::cols.pred - heading_1.size - 2) / 2)
     @main_window << heading_1
@@ -73,13 +112,14 @@ class TerminalGUI
     @main_window.setpos(3, (Curses::cols.pred - heading_2.size - 2) / 2)
     @main_window << heading_2
 
-    @main_window.attroff(Curses::color_pair(pair["Title"]))
+    @main_window.attroff(Curses::color_pair(@pairs["Title"]))
     @main_window.refresh
   end
 
   def stop
     @subwindow.close unless @subwindow.nil?
     @main_window.close unless @main_window.nil?
+    Curses::close_screen
   end
 
   def render_world
@@ -92,13 +132,14 @@ class TerminalGUI
 
     initialize_color_pairs
 
-    pair = {"Title" => 1, "Tile" => 2, "Water" => 3, "Wall" => 4, "Player" => 5}
+    @pairs = {"Title" => 1, "Tile" => 2,
+              "Water" => 3, "Wall" => 4, "Player" => 5}
 
     @main_window = Curses::Window.new(0, 0, 0, 0)
     @main_window.box('#', '-')
     @main_window.refresh
 
-    add_titles(heading_1, heading_2, pair)
+    add_titles(heading_1, heading_2)
 
     @subwindow = @main_window.subwin(@world.height, (@world.width * 2), 4,
                                      (Curses::cols - 2 - @world.width * 2) / 2)
@@ -108,26 +149,18 @@ class TerminalGUI
         
         @subwindow.setpos(j, i * 2)
         tile = @world[i, j].class.to_s
-        case tile
-        when "Tile"
-          @subwindow.attron(Curses::color_pair(pair[tile]))
-          @subwindow << @render_palette[tile]
-        when "Wall"
-          @subwindow.attron(Curses::color_pair(pair[tile]))
-          @subwindow << @render_palette[tile]
-        when "Water"
-          @subwindow.attron(Curses::color_pair(pair[tile]))
-          @subwindow << @render_palette[tile]
-        end
+        fail if @render_palette[tile].nil?
+        @subwindow.attron(Curses::color_pair(@pairs[tile]))
+        @subwindow << @render_palette[tile]
         
-        @subwindow.attroff(Curses::color_pair(pair[tile]))
+        @subwindow.attroff(Curses::color_pair(@pairs[tile]))
       end
     end
 
-    render_player(@subwindow, pair)
-
+    # render_player(@subwindow, @pairs)
+    render_pixel(@subwindow, @world.player.x, @world.player.y, "Player")
     @subwindow.refresh
-    @subwindow.getch
+    @main_window.refresh
   end
 
 end
